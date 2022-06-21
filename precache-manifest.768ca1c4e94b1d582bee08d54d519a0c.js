164f67891391ca38454b5bcc0fb5a5f0c5792122
Jonathan Choi <51044058+koreaunitas@users.noreply.github.com>
2022-06-22 00:11:06 +0900

    Updates

---
+++ precache-manifest.768ca1c4e94b1d582bee08d54d519a0c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "27e56ad030cad9b288cc",
+    "url": "/ai-ethics/static/js/main.27e56ad0.chunk.js"
+  },
+  {
+    "revision": "301350276c9b2558dbc2",
+    "url": "/ai-ethics/static/js/1.30135027.chunk.js"
+  },
+  {
+    "revision": "af1ee1d6ac33974cbf92",
+    "url": "/ai-ethics/static/js/runtime~main.af1ee1d6.js"
+  },
+  {
+    "revision": "d3f0e4274216bd3fd02a1c0823132544",
+    "url": "/ai-ethics/static/media/0315-1.d3f0e427.png"
+  },
+  {
+    "revision": "826a0d76ebd805453f66c31822860403",
+    "url": "/ai-ethics/static/media/0315-2.826a0d76.png"
+  },
+  {
+    "revision": "b185fc31405536f06e1b3ce486091b42",
+    "url": "/ai-ethics/static/media/0315-3.b185fc31.png"
+  },
+  {
+    "revision": "a3496f492a4d0f6dd35c81f105a393a6",
+    "url": "/ai-ethics/static/media/0331-1.a3496f49.png"
+  },
+  {
+    "revision": "badd1be9c6f07af36115bead3158b6cf",
+    "url": "/ai-ethics/static/media/0331-2.badd1be9.png"
+  },
+  {
+    "revision": "d4709c6a4324c897da2a989d39170a3d",
+    "url": "/ai-ethics/static/media/0331-3.d4709c6a.png"
+  },
+  {
+    "revision": "b6f931434cc1cf7a0454d1a2388c4b35",
+    "url": "/ai-ethics/index.html"
+  }
+];
